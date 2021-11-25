c556a9a7ac0033abe8b8eb8c7f78d712d446ed77
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-25 22:57:54 +0300

    Name and content changed of main activity

---
+++ app/src/main/java/com/oguzdogdu/simplecaching/features/restaurants/RestaurantActivity.kt
@@ -0,0 +1,12 @@
+package com.oguzdogdu.simplecaching.features.restaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oguzdogdu.simplecaching.R
+
+class RestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+    }
+}
