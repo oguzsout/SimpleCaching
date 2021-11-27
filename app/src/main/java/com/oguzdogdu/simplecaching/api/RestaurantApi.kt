20ca0141933b9ca9ffb297d72e08bf99d8318814
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-28 00:09:47 +0300

    Api service created

---
+++ app/src/main/java/com/oguzdogdu/simplecaching/api/RestaurantApi.kt
@@ -0,0 +1,13 @@
+package com.oguzdogdu.simplecaching.api
+
+import com.oguzdogdu.simplecaching.data.Restaurant
+import retrofit2.http.GET
+
+interface RestaurantApi {
+    companion object {
+        const val BASE_URL = "https://random-data-api.com/api/"
+    }
+
+    @GET("restaurant/random_restaurant?size=20")
+    suspend fun getRestaurants(): List<Restaurant>
+}
