c3a246700ce53bd92c800d38b22e8127dfe2d1b4
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-28 20:30:53 +0300

    Adapter set in main activity

---
+++ app/src/main/java/com/oguzdogdu/simplecaching/RestaurantApplication.kt
@@ -0,0 +1,8 @@
+package com.oguzdogdu.simplecaching
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RestaurantApplication : Application() {
+}
--- app/src/main/java/com/oguzdogdu/simplecaching/features/restaurants/RestaurantActivity.kt
+++ app/src/main/java/com/oguzdogdu/simplecaching/features/restaurants/RestaurantActivity.kt
@@ -1,12 +1,33 @@
 package com.oguzdogdu.simplecaching.features.restaurants
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.oguzdogdu.simplecaching.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.oguzdogdu.simplecaching.databinding.ActivityRestaurantBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RestaurantActivity : AppCompatActivity() {
+
+    private val viewModel: RestaurantViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restaurant)
+        val binding = ActivityRestaurantBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val restaurantAdapter = RestaurantAdapter()
+
+        binding.apply {
+            recyclerView.apply {
+                adapter = restaurantAdapter
+                layoutManager = LinearLayoutManager(this@RestaurantActivity)
+            }
+
+            viewModel.restaurants.observe(this@RestaurantActivity) { restaurants ->
+                restaurantAdapter.submitList(restaurants)
+            }
+        }
     }
 }
