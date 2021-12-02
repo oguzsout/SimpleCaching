7a68bf19bb32e30df5df502a4a8ef49ceb4c1e97
Oguzhan Dogdu <oguzd494@gmail.com>
2021-12-02 20:10:00 +0300

    RestaurantDao.kt created

--- app/src/main/java/com/oguzdogdu/simplecaching/data/Restaurant.kt
+++ app/src/main/java/com/oguzdogdu/simplecaching/data/Restaurant.kt
@@ -1,6 +1,11 @@
 package com.oguzdogdu.simplecaching.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "restaurants")
 data class Restaurant(
+    @PrimaryKey
     val name: String,
     val type: String,
     val logo: String,
